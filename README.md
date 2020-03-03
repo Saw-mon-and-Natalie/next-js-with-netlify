98d21eb43e869b77750d739a88a1bcdb225ee322
Saw-mon-and-Natalie <samangharib50233@gmail.com>
2020-03-02 17:35:46 -0800

    Setting up a simple NextJS to test Netlify Large Media.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Setup
+
+```bash
+netlify login
+netlify link
+
+netlify lm:setup
+
+git lfs track <FILE_NAMES>
+```
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+const Index = () => (
+    <div>
+        <h1>NextJS test app on Netlify</h1>
+        <ul>
+            <li>testing large media format</li>
+        </ul>
+        <img src="/assets/1.jpg" width={400} height={561} />
+        <img src="/assets/subfolder/0.jpg" width={600} height={450} />
+    </div>
+)
+
+export default Index
